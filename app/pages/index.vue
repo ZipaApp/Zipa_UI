213a9a55080b2a8193ee5779ca20576163c7e3e1
Fabian Torres <fatorresra@unal.edu.co>
2025-10-01 17:00:18 -0500

    Initialize Nuxt.js application with starting pages

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Bienvenido a la tienda Zipa!</h1>
+        <p>Aquí podrás encontrar una variedad de productos y servicios para tu mascota.</p>
+    </div>
+</template>
---
+++ app/pages/services.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Servicios para tu mascota</h1>
+    </div>
+</template>
